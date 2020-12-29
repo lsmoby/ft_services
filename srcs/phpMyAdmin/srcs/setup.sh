ab5a9eb72a67625ed9718b7213ab2d7866cf174a
Anouar El ghemari <ael-ghem@e2r2p6.1337.ma>
2020-12-29 11:09:36 +0100

    phpmyadmin image finished

--- srcs/phpMyAdmin/srcs/setup.sh
+++ srcs/phpMyAdmin/srcs/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-
 mkdir -p /run/openrc/
 touch /run/openrc/softlevel
 openrc
 rc-service nginx start
 rc-service nginx stop
 rc-service php-fpm7 start
-#sed -i 's/#cgi.fix_pathinfo=1/#cgi.fix_pathinfo=0/' /etc/php7/php.ini
+cp /var/www/html/config.sample.inc.php /var/www/html/config.inc.php
+sed -i 's/$cfg['Servers'][$i]['host'] = 'localhost';/$cfg['Servers'][$i]['host'] = '$MYSQL_SERVICE_PORT_3306_TCP_ADDR';/' /var/www/html/config.inc.php
 rc-service php-fpm7 restart
 nginx -g "daemon off;"
