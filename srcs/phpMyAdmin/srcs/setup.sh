2d75b28206fe8b246372d89b2a7a486b6c085640
Anouar El ghemari <ael-ghem@e2r2p6.1337.ma>
2020-12-29 11:01:40 +0100

    phpmyadmin almost done

--- srcs/phpMyAdmin/srcs/setup.sh
+++ srcs/phpMyAdmin/srcs/setup.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
 
-addgroup -g 82 -S www-data; \
-
-mkdir -p var/www/html
-
-#create users for php and nginx
-adduser -D -g 'www-data' www-data
-
-#setting up nginx webserver user permissions to directories
-mkdir -p /usr/local/etc/php/conf.d; 
-mkdir -p /var/www/html;
-chown -R www-data:www-data /var/www/html;
-chmod 777 /var/www/html
-chown -R www-data:www-data /var/lib/nginx
 
 mkdir -p /run/openrc/
 touch /run/openrc/softlevel
-sed -i 's/#cgi.fix_pathinfo=1/#cgi.fix_pathinfo=0/' /etc/php7/php.ini
 openrc
+rc-service nginx start
+rc-service nginx stop
 rc-service php-fpm7 start
-nginx
+#sed -i 's/#cgi.fix_pathinfo=1/#cgi.fix_pathinfo=0/' /etc/php7/php.ini
+rc-service php-fpm7 restart
 nginx -g "daemon off;"
