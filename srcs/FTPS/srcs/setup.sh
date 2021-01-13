52cebb4668add15b13a7e06017f602c6fdbfebcf
Anouar El ghemari <ael-ghem@e2r2p6.1337.ma>
2021-01-13 12:56:14 +0100

    fuck ftps

--- srcs/FTPS/srcs/setup.sh
+++ srcs/FTPS/srcs/setup.sh
@@ -1,41 +1,15 @@
-mkdir -p /etc/ssl/private /etc/ssl/certs/
-openssl req -x509 -nodes -days 720 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/certs/vsftpd.pem -subj "/C=MA/ST=BENGUERIR/L=1337/O=ael-ghem/OU=IT Department/CN=Archi"
-echo -e "anonymous_enable=YES
-local_enable=YES
-write_enable=YES
-local_umask=022
-dirmessage_enable=YES
-xferlog_enable=YES
-connect_from_port_20=YES
-ftpd_banner=Welcome to FTPS server from the ft_services 42 project.
-listen=YES
-seccomp_sandbox=NO
-rsa_cert_file=/etc/ssl/certs/vsftpd.pem
-rsa_private_key_file=/etc/ssl/private/vsftpd.pem
-ssl_tlsv1=NO
-ssl_sslv2=NO
-ssl_sslv3=NO
-ssl_enable=YES
-pasv_address=$PASVADDR
-pasv_enable=YES
-pasv_max_port=30009
-pasv_min_port=30000
-force_local_data_ssl=YES
-force_local_logins_ssl=YES
-require_ssl_reuse=NO
-ssl_ciphers=HIGH:MEDIUM"  > /etc/vsftpd/vsftpd.conf
+#!/bin/sh
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/pure-ftpd.pem -out /etc/ssl/private/pure-ftpd.pem -subj "/C=MA/ST=benguerir/L=maroc/O=ael-ghem/OU=1337/CN=localhost"
+chmod 600 /etc/ssl/private/pure-ftpd.pem
+echo -e "TLS                         2
+TLSCipherSuite               HIGH:MEDIUM:+TLSv1:!SSLv2:!SSLv3
+CertFileAndKey    \"/etc/ssl/private/pure-ftpd.pem\" ">> /etc/pure-ftpd.conf
 
 if [[ ! -z "$FTP_USER" ]] && [[ ! -z "$FTP_PASS" ]]
 then
-    addgroup -g 433 -S $FTP_USER
-    adduser -u 431 -D -G $FTP_USER -h /home/$FTP_USER -s /bin/false  $FTP_USER
+    adduser -D $FTP_USER
     echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
-    chown $FTP_USER:$FTP_USER /home/$FTP_USER/ -R
-    echo FTPS_USER = $FTP_USER
 fi
 
-chmod 600 /etc/ssl/certs/vsftpd.pem
-chmod 600 /etc/ssl/private/vsftpd.pem
-
-
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+pure-ftpd -j -Y 2 -p 30000:30009 -P $PASV_ADDRESS
---
+++ srcs/Grafana/srcs/setup.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+
+/opt/grafana/bin/grafana-server
