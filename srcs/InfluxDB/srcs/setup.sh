d0b7026b74f90c87e29c18f7b8b1adbac17916d6
Anouar El ghemari <ael-ghem@e2r2p4.1337.ma>
2021-01-16 18:13:03 +0000

    liveness prob added to influxdb and tested

--- srcs/InfluxDB/srcs/setup.sh
+++ srcs/InfluxDB/srcs/setup.sh
@@ -1,6 +1,15 @@
-mkdir -p /usr/src /etc/telegraf
-wget https://dl.influxdata.com/telegraf/releases/telegraf-1.17.0_linux_amd64.tar.gz
-tar -C /-xzf telegraf-1.17.0_linux_amd64.tar.gz
+#!/bin/bash
+sh /telegraf.sh
+(influxd &) && ( /telegraf-1.17.0/usr/bin/telegraf --config /etc/telegraf/telegraf.conf &)
 
-chmod +x /telegraf*/*
-cp -a /telegraf*/* /
+mkdir /liveness
+touch /liveness/live
+
+while true;	do
+ps > /liveness/processes && cat /liveness/processes | grep "/telegraf-1.17.0/usr/bin/telegraf --config /etc/telegraf/telegraf.conf" ; [ $? -eq 1 ] && touch /liveness/teleg_
+cat /liveness/processes | grep "influx"; [ $? -eq 1 ] && touch /liveness/influx_
+if [ ! -f /liveness/influx_ -o ! -f /liveness/teleg_ ]; then
+    rm /liveness/live
+fi
+sleep 5
+done
